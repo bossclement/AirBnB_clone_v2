bd781dce3e0ae72127103d5c33c70e3bd9c3b98f
bossclement <bossclement@gmail.com>
2023-08-26 17:16:20 +0200

    New commit

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,3 +1,4 @@
+-- Creates a database on localhost server and grants it some privileges
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
