d6f9f2d8491a008e347dd9fffaa711a961bc0797
bossclement <bossclement@gmail.com>
2023-09-24 18:58:50 +0200

    New commit

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 """Starts a simple flask webserver"""
 from flask import Flask
-
 app = Flask(__name__)
+
 
 @app.route("/", strict_slashes=False)
 def home():
     """Displays the text below on the browser"""
     return "Hello HBNB!"
+
 
 if __name__ == "__main__":
     app.run()
