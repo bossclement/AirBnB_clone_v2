4c3bb4f55e78ed7114f897238ca9812cbf33432b
bossclement <bossclement@gmail.com>
2023-09-24 19:16:27 +0200

    New commit

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Starts a simple flask webserver"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def home():
+    """Displays the text below on the browser"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Displays the text below on the browser"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def text(text):
+    """Displays c followed with text stored in text variable"""
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route("/python/", defaults={'text': 'is_cool'})
+@app.route("/python/<text>", strict_slashes=False)
+def python(text):
+    """Displays Python followed with text stored in text variable"""
+    return "Python {}".format(text.replace("_", " "))
+
+
+@app.route("/number/<n>")
+def number(n):
+    """Displays n if it's a number"""
+    return "{} is a number".format(n)
+
+
+if __name__ == "__main__":
+    app.run()
